90588f6d91c48b7199f33af803d8ace60650c8aa
iVan <ivan.peng@skiff.com>
2023-09-25 19:22:52 +0800

    changed

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230925.191700';
+const cacheName = 'chirpy-20230925.192201';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
