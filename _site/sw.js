0cff74e897e643480a1ac0deffc94b0be283af1d
iVan <ivan.peng@skiff.com>
2023-09-25 19:45:12 +0800

    update .gitignore

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230925.193411';
+const cacheName = 'chirpy-20230925.194438';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
